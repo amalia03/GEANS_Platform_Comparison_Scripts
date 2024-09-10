de1bb8cb7d9415619f84297b6e07814cd3a35b99
Amalia M. <a.mailli@yahoo.com>
2024-09-10 17:19:33 +0200

    Update tax_geans.R

--- tax_geans.R
+++ tax_geans.R
@@ -1,35 +1,31 @@
-library(vegan)
-library(MASS)
-source("tax_viz_functions.R")
+#Locate the BLAST files (here it assumes it is in the same directory and they have the names written as such "X_reads.bl")
+bl.files <- list.files("./","reads.bl$")
 
-bl.files <- list.files("../contig_vs_GEANS_dtst/","reads.bl$")
-
+#Import the files in a list
 bl.l <- lapply(1:length(bl.files), function(x){
     read.delim(paste0("../contig_vs_GEANS_dtst/",bl.files[x]), header=F,sep="\t", stringsAsFactors=FALSE, quote= "" )
 })
 
+#Use the file name as an ID
 sample.names <- sub("([0-9A-Z]+).*", "\\1",bl.files)
 sample.names 
 
 names(bl.l) <- sample.names
-##Remove renegade column and rename the files
 
+#Name BLAST columns
 for(x in 1:length(bl.l)){    
      colnames(bl.l[[x]]) <- c('query', 'subject', 'title', 'qlen', 'slen', 'qstart', 'qend', 'sstart', 'ssend','evalue', 'score', 'length', 'pident', 'nident', 'gapopen', 'gaps', 'qcovs')
 }
-unlist(lapply(bl.l, function(x){length(unique(x$query))}))
-
-
-#bl.l <- lapply(bl.l, function(x){x[x$qlen>150& x$length>=100,]})
+#Enforce alignment quality filters (alignment length and/or percent similarity)
 bl.l <- lapply(bl.l, function(x){x[x$length>=100,]})
-unlist(lapply(bl.l, function(x){length(unique(x$query))}))
 
+#For each query, remove matches that aligned to more than one phylum (high taxonomic ambiguity)
 m.ph <- sapply(unique(bl.l$query), function(x){length(unique(bl.l[bl.l$query==x,"phylum"]))})
 print(paste0("Proportion of single phyla per query sequence ", round((sum(m.ph[m.ph==1])/length(m.ph))*100, 2), "%"))
 s.ph <- m.ph[m.ph==1]
 tax.info.sph <- bl.l[bl.l$query %in% names(s.ph), ]
 
-
+#Keep only the top scoring (bitscore) match per contig
 bl.s.top <- lapply(1:length(bl.l),function(x){
     bl.s.i <- bl.l[[x]]
         bl.s.sc <- tapply(1:nrow(bl.s.i), bl.s.i$query, function(i){
@@ -40,6 +36,7 @@ bl.s.top <- lapply(1:length(bl.l),function(x){
     bl.s.ts
 })
 
+#Make a genus and species column based on the match identifiers (which usually contain the complete taxonomy of each match)
 bl.tax <- lapply(bl.s.top, function(x){
     x.tax <- strsplit(x$subject, ";")
     x$genus <- unlist(lapply(x.tax, function(y){
@@ -50,20 +47,22 @@ bl.tax <- lapply(bl.s.top, function(x){
     x
 })
 
+#Rename certain species with defunct/incorrect appelations 
 bl.tax <- lapply(bl.tax, function(x){
     x[x$species=="Limecola balthica", "species"] <- "Macoma balthica"
     x[x$species=="Owenia fisiformis", "species"] <- "Owenia fusiformis"
     x[x$species=="Sagartia elegans", "species"] <- "Cylista elegans"
+     x[x$species=="Tritia reticulatus", "species"] <- "Tritia reticulata" 
     x
 })
 
+##Set a limit to the lowest number of reads matching to each species (here set to 5 matches per species)
 bl.tax.t <- lapply(bl.tax, function(x){table(x$species)})
 bl.tax.t <- lapply(bl.tax.t, function(x){x[x>=5]})
 bl.tax.t
 unlist(lapply(bl.tax.t, sum))
 
-unique(names(unlist(lapply(1:3, function(x){bl.tax.t[[x]]}))))
-
+##Create an abundance table for each species per location
 all.tax <- unique(unlist(lapply(bl.tax.t, names)))
 gr.matrix <- matrix(0, nrow=length(all.tax), ncol=12)
 rownames(gr.matrix) <- sort(all.tax)
@@ -76,4 +75,5 @@ for(i in 1:length(sample.names)){
         }}
 }
 
+#Export the matrix in a table.
 write.table(gr.matrix, "geans_reads.tsv", sep="\t", quote=F) 
